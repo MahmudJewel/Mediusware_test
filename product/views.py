072b9d581300506b599eb1384b67dfcf9f27b3c9
jewel <jmbengal.mbstu@gmail.com>
2022-01-02 02:21:55 +0600

    pagination done

--- product/views.py
+++ product/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from product import models
+# for paginations 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def home_view(request):
-    pd = models.Product.objects.get(id=1)
-    pvprice= models.ProductVariantPrice.objects.filter(product=pd)
-    print(f"price: {pvprice}")
-    vrprice=models.Product.objects.get(id=1).productvariantprice_set.all()
-    variant=models.Product.objects.get(id=1).productvariant_set.all()
-    stock=models.Product.objects.get(id=1).productvariant_set.all()
-    print(f"variantprice: {vrprice} \n variant: {variant}\nstock: ")
-    Products=models.Product.objects.all()
-    # print(f"Products are: {Products.productvariantprice_set.all()}")
+    # pd = models.Product.objects.get(id=1)
+    # pvprice= models.ProductVariantPrice.objects.filter(product=pd)
+    # print(f"price: {pvprice}")
+    # vrprice=models.Product.objects.get(id=1).productvariantprice_set.all()
+    # variant=models.Product.objects.get(id=1).productvariant_set.all()
+    # stock=models.Product.objects.get(id=1).productvariant_set.all()
+    # print(f"variantprice: {vrprice} \n variant: {variant}\nstock: ")
+    products=models.Product.objects.all()
+    product_num = products.count()
+    
+    # for paginations 
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products, 2)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     context={
-        'Products':Products,
+        'products':products,
+        'product_num':product_num,
     }
     return render(request, 'home.html',context)
