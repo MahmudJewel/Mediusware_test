b60bf56281d8250be51a8f79e3bf0570d503ee75
jewel <jmbengal.mbstu@gmail.com>
2022-01-02 05:58:35 +0600

    chaining filter added on search

--- product/views.py
+++ product/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from product import models
 # for paginations 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# for complex query 
+from django.db.models import Q
 
 # Create your views here.
 def home_view(request):
@@ -34,11 +36,29 @@ def home_view(request):
     }
     return render(request, 'home.html',context)
 
+# search bar 
 def search_view(request):
     if request.method == 'GET':
-        search = request.GET.get('inputTitle')
-        # print(f"search : {search}")
-        searchItem = models.Product.objects.filter(title__contains = search)
+        search = request.GET.get('inputTitle') # get the title from search
+        dropdown = request.GET.get('dropdown') # get the choice from search
+        priceFrom = request.GET.get('from') # get the price from search
+        priceTo = request.GET.get('to')
+        # converting string to int & for empty logic applied 
+        if (priceFrom !=''):
+            priceFrom = int(priceFrom)
+        else:
+            priceFrom = 0
+        if (priceTo !=''):
+            priceTo = int(priceTo)
+        else:
+            priceTo = 9999999
+            
+        # print(f"search : {type(priceFrom)}")
+        # searchItem = models.Product.objects.filter(title__contains = search)
+        # searchItem = models.Product.objects.filter(productvariant__variant__title = dropdown)
+        
+        # chaining filter 
+        searchItem = models.Product.objects.filter(productvariantprice__price__gte = priceFrom, productvariantprice__price__lte = priceTo).filter(title__contains = search).filter(productvariant__variant__title = dropdown)
         context = {
             'products':searchItem,
         }
