3ae3dd52e893d5c5f4111e2aca91643990b7eb83
jewel <jmbengal.mbstu@gmail.com>
2022-01-01 13:48:02 +0600

    five model added

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from product import models
 # Register your models here.
+admin.site.register(models.Variant)
+admin.site.register(models.Product)
+admin.site.register(models.ProductImage)
+admin.site.register(models.ProductVariant)
+admin.site.register(models.ProductVariantPrice)
+
