1727716582ce2664ad2501830a995b1e1c737b37
jewel <jmbengal.mbstu@gmail.com>
2022-01-01 11:02:56 +0600

    template folder added

--- Mediusware_test/settings.py
+++ Mediusware_test/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Mediusware_test.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
